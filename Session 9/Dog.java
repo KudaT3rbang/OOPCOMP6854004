edc86c8765c2973f239dac9d618050a83199ef8e
nathan <nathanaelgilberth353@gmail.com>
2024-11-22 16:40:40 +0700

    Session 9 Commit

---
+++ Session 9/Dog.java
@@ -0,0 +1,13 @@
+class Dog implements Pet, Animal {
+    public void eat() {
+        System.out.println("Dogs eat meat");
+    }
+
+    public void sleep() {
+        System.out.println("Dogs sleep 8 hours a day");
+    }
+
+    public void showAffection() {
+        System.out.println("Dogs whips it tails");
+    }
+}
